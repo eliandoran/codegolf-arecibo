5c5ab7d26d255e835cd0ebe08ca7f192479fa95e
Elian Doran <elinnum@gmail.com>
2016-11-25 20:21:38 +0200

    Simplify variable & method names

--- src/arecibo.min.cpp
+++ src/arecibo.min.cpp
@@ -1,10 +1,10 @@
 #include <fstream>
 using namespace std;
 
-char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,./:;<=>?@[]^_`{|}~'";
-char data[] = "AAKusAB([;AF`ZTAHmdqAAAAAAAAQ@AAA/NAAA/NAABbOAAB^mAAAAAAIt,eAFzx7AJKQ1AK{^aAAAAAAA#cnAAAAAAAf7%AK,fNAAAAAAIr<JAF1rrAJHMsAK{^aAAAAAAA$E{AAA(vAAgkVAK.H8AAA(vABj?hAA$FBAAwU)AAML;AAA,(AAML;AAwU)AA#/uABkhiAC,'RAC,tUABkLAAA#dqAAvwZAByOBABkK;ABnp7ABpjGAAJ!UAIB;dAABWQABk?DABk>?ABm|6AAAAAACoH`ACx7NACn58AAADvAAD;<AAP.xAA3pOABD~PACX,uAEn8pADAKRAC~R+AARHaAAQe+AAP<*AAE0gAFb1P";
+char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,./:;<=>?@[]^_`{|}~'";
+char d[] = "AAKusAB([;AF`ZTAHmdqAAAAAAAAQ@AAA/NAAA/NAABbOAAB^mAAAAAAIt,eAFzx7AJKQ1AK{^aAAAAAAA#cnAAAAAAAf7%AK,fNAAAAAAIr<JAF1rrAJHMsAK{^aAAAAAAA$E{AAA(vAAgkVAK.H8AAA(vABj?hAA$FBAAwU)AAML;AAA,(AAML;AAwU)AA#/uABkhiAC,'RAC,tUABkLAAA#dqAAvwZAByOBABkK;ABnp7ABpjGAAJ!UAIB;dAABWQABk?DABk>?ABm|6AAAAAACoH`ACx7NACn58AAADvAAD;<AAP.xAA3pOABD~PACX,uAEn8pADAKRAC~R+AARHaAAQe+AAP<*AAE0gAFb1P";
 
-int pow(int n,int e){if(e<=0)return 1;return n*pow(n, e-1);}
+int p(int n,int e){if(e<=0)return 1;return n*p(n, e-1);}
 int i,j,k,nr;
 int main()
 {
@@ -13,8 +13,8 @@ int main()
     for (;i<365;i+=5) {
         nr=0;
         for (j=0;j<5;j++) {
-            for (k=0;k<366;k++)if(alphabet[k]==data[i+4-j])break;
-            nr+=k*pow(91, j);
+            for (k=0;k<366;k++)if(a[k]==d[i+4-j])break;
+            nr+=k*p(91, j);
         }
 
         for (j=22;j>=0;j--)f<<(nr>>j&1);
