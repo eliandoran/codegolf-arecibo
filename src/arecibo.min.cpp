88ac6726447920e2bc907b15d57a98abfa3ed6e0
Elian Doran <elinnum@gmail.com>
2016-11-25 20:24:09 +0200

    Remove default namespace

--- src/arecibo.min.cpp
+++ src/arecibo.min.cpp
@@ -1,6 +1,4 @@
 #include <fstream>
-using namespace std;
-
 char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,./:;<=>?@[]^_`{|}~'";
 char d[] = "AAKusAB([;AF`ZTAHmdqAAAAAAAAQ@AAA/NAAA/NAABbOAAB^mAAAAAAIt,eAFzx7AJKQ1AK{^aAAAAAAA#cnAAAAAAAf7%AK,fNAAAAAAIr<JAF1rrAJHMsAK{^aAAAAAAA$E{AAA(vAAgkVAK.H8AAA(vABj?hAA$FBAAwU)AAML;AAA,(AAML;AAwU)AA#/uABkhiAC,'RAC,tUABkLAAA#dqAAvwZAByOBABkK;ABnp7ABpjGAAJ!UAIB;dAABWQABk?DABk>?ABm|6AAAAAACoH`ACx7NACn58AAADvAAD;<AAP.xAA3pOABD~PACX,uAEn8pADAKRAC~R+AARHaAAQe+AAP<*AAE0gAFb1P";
 
@@ -8,7 +6,7 @@ int p(int n,int e){if(e<=0)return 1;return n*p(n, e-1);}
 int i,j,k,nr;
 int main()
 {
-    ofstream f("arecibo.txt");
+    std::ofstream f("arecibo.txt");
 
     for (;i<365;i+=5) {
         nr=0;
@@ -18,7 +16,7 @@ int main()
         }
 
         for (j=22;j>=0;j--)f<<(nr>>j&1);
-        f<<endl;
+        f<<'\n';
     }
 
     return 0;
