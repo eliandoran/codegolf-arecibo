0db0e6753a53f5f6ec9ab60cc911db7250107a36
Elian Doran <elinnum@gmail.com>
2016-11-25 20:10:58 +0200

    Integrate string search algorithm into main

--- src/arecibo.min.cpp
+++ src/arecibo.min.cpp
@@ -5,21 +5,19 @@ char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012345678
 
 char data[] = "AAKusAB([;AF`ZTAHmdqAAAAAAAAQ@AAA/NAAA/NAABbOAAB^mAAAAAAIt,eAFzx7AJKQ1AK{^aAAAAAAA#cnAAAAAAAf7%AK,fNAAAAAAIr<JAF1rrAJHMsAK{^aAAAAAAA$E{AAA(vAAgkVAK.H8AAA(vABj?hAA$FBAAwU)AAML;AAA,(AAML;AAwU)AA#/uABkhiAC,'RAC,tUABkLAAA#dqAAvwZAByOBABkK;ABnp7ABpjGAAJ!UAIB;dAABWQABk?DABk>?ABm|6AAAAAACoH`ACx7NACn58AAADvAAD;<AAP.xAA3pOABD~PACX,uAEn8pADAKRAC~R+AARHaAAQe+AAP<*AAE0gAFb1P";
 
-int get_ord(char ch) {
-    int i=0; for (; i<366; i++) if (alphabet[i] == ch) break; return i;
-}
-
 int pow(int n, int e) { if (e <= 0) return 1; return n * pow(n, e-1); }
 
-int i,j,nr;
+int i,j,k,nr;
 int main()
 {
     ofstream f("arecibo.txt");
 
     for (i=0; i < 365; i+=5) {
         nr=0;
-        for (j=0; j < 5; j++)
-            nr+=get_ord(data[i+4-j]) * pow(91, j);
+        for (j=0; j < 5; j++) {
+            for (k=0; k<366; k++) if (alphabet[k] == data[i+4-j]) break;
+            nr+=k * pow(91, j);
+        }
 
         for (j=22; j>=0; j--) {
             int bit = (nr >> j) & 1;
