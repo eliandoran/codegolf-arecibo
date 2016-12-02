3d1f2b85a645e65d2fcb92137743f508ac3f616a
Elian Doran <elinnum@gmail.com>
2016-12-02 22:36:33 +0200

    Use explicit std notation

--- src/arecibo.min.cpp
+++ src/arecibo.min.cpp
@@ -1,9 +1,8 @@
 #include <fstream>
-using namespace std;
 
 char data[] = "#MC#-%C'E+H5MML'######S##&3##&3##(3##*S#####S[S;C#/3W;S=aaaB####'##%####%##$a##B####SS[;C+#3W/?=aaaB####'&#%#&##%&#$a&#B#&##+$#''&#+&&#3#T&##&/##T&#&&#3'%#++&#'3&#'3$#++%#3'#&#&#/#+]S#+%##+2C#+:5>#J,BQ*$:#($>+($B+($3+0C#####1%##1KM81#-7##-##2C##b[#&C1#)#&#0#(3<C/;437+4+G+$('#$%'#$#'##5C#AB6;";
 
-ofstream f("arecibo.txt");
+std::ofstream f("arecibo.txt");
 
 int main()
 {
@@ -14,6 +13,6 @@ int main()
             f<<((num >> j) & 1);
 
         if (i % 4 == 3)
-            f<<endl;
+            f<<'\n';
     }
 }
